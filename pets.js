9665719fbe82e7b136ac85bdbe2e6b7b5168222e
Geraldo Moura <geraldo.moura@ymail.com>
2021-09-22 15:49:42 -0500

    commit before wrinting on file

--- pets.js
+++ pets.js
@@ -1,5 +1,5 @@
 const { argv, exit } = require('process');
-const { readFile } = require('fs/promises');
+const { readFile, writeFile } = require('fs/promises');
 
 
 async function main() {
@@ -7,23 +7,46 @@ async function main() {
     const string = await readFile('./pets.json', 'utf8');
     const data = JSON.parse(string);
 
+    // Writing data to pets.json
+    // const writingOnFile = (toInsert, file) => await writeFile(toInsert, file);
+
     // Reading commands from terminal
-    const command = argv[2]
-    const indexRead = argv[3]
+    const command = argv[2];
+    const indexRead = argv[3];
 
     if (command === undefined) {
         console.error('Usage: node pets.js [read | create | update | destroy]')
+        return exit(1)
     }
 
     // const readingFile = (action, i) => {
-    //     return action === 'read' ? 
+    //     return (action === 'read' && indexRead) === undefined ? console.log(data)
+    //         : (indexRead !== undefined)
     // }
 
     if (command === 'read' && indexRead === undefined) {
         console.log(data)
-    } else if (indexRead !== undefined) {
-        console.log(data[indexRead])
+        return exit(1)
+    } else if (command === 'read' && indexRead) {
+        if (data[indexRead] === undefined) {
+            console.error('Usage: node pets.js read INDEX')
+            return exit(1)
+        } else {
+            console.log(data[indexRead])
+        }
     }
+
+    if (command === 'create') {
+        let age = argv[3];
+        let kind = argv[4];
+        let name = argv[5];
+        let newObj = { age: age, kind: kind, name: name };
+
+        let newData = { ...data, ...newObj }
+
+        console.log(newData)
+    }
+
 }
 
 // const command = argv[2]
