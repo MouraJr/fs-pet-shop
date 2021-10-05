006eda367343a8b846e3414d87f88b73e65e0c49
Geraldo Moura <geraldo.moura@ymail.com>
2021-10-05 12:21:50 -0500

    restful redone

--- readJson.js
+++ readJson.js
@@ -1,12 +1,9 @@
 const { readFile } = require('fs/promises');
 
-const readData = async () => {
-    try {
-        const data = await readFile('./pets.json', 'utf8');
-        return data
-    } catch (error) {
-        console.log((error.message))
-    }
+// Reading pets.json file and parsing to JSON format
+const readFunction = async () => {
+    const string = await readFile('./pets.json', 'utf8');
+    return data = JSON.parse(string);
 }
 
-module.exports = readData;
+module.exports = readFunction;
--- restfulExpressServer.js
+++ restfulExpressServer.js
@@ -1,5 +1,7 @@
-var express = require('express')
-var morgan = require('morgan')
+const express = require('express')
+const morgan = require('morgan')
+const { writeFile } = require('fs/promises')
+const readFunction = require('./readJson')
 
 var app = express()
 
@@ -9,9 +11,78 @@ app.use(morgan('combined'))
 // Body Parser Middleware
 app.use(express.json());
 
-app.get('/', function (req, res) {
-    res.send('hello, world!')
+// Get the data
+app.get('/pets', async (req, res) => {
+    await readFunction();
+    res.set('Content-Type', 'application/json')
+    res.send(data)
 })
+
+// Get pet by index
+app.get('/pets/:index', async (req, res) => {
+    await readFunction();
+    // Check if :index exist in data[index] 
+    const found = data.some(pet => data.indexOf(pet) === parseInt(req.params.index));
+
+    // Conditional if exist
+    if (found) {
+        res.set('Content-Type', 'application/json')
+        res.json(data.find(pet => data.indexOf(pet) === parseInt(req.params.index)));
+        res.end();
+    } else if (!found) {
+        res.set('Content-Type', 'text/plain')
+        res.status(404);
+        res.end('Not Found');
+    }
+});
+
+// POST the data
+app.post('/pets', async (req, res) => {
+    await readFunction();
+    const newPet = {
+        name: req.body.name,
+        age: req.body.age,
+        kind: req.body.kind
+    }
+
+    if (!newPet.name || !newPet.age || !newPet.kind) {
+        return res.status(400).end('Please include a name, age, and kind to create!')
+    }
+
+    data.push(newPet)
+    // Writing data to pets.json
+    await writeFile('pets.json', JSON.stringify(data));
+    res.json(newPet)
+})
+
+// PATCH data
+app.patch('/pets/:index', async (req, res) => {
+    await readFunction();
+    // Check if :index exist in data[index] 
+    const found = data.some(pet => data.indexOf(pet) === parseInt(req.params.index));
+
+    // Conditional if exist
+    if (found) {
+        const patchPet = req.body;
+        data.forEach(pet => {
+            if (data.indexOf(pet) === parseInt(req.params.index)) {
+                data.name = patchPet.name ? patchPet.name : data.name;
+                data.age = patchPet.age ? patchPet.age : data.age;
+                data.kind = patchPet.kind ? patchPet.kind : data.kind;
+            }
+            console.log(data)
+        })
+        // Writing data to pets.json
+        await writeFile('pets.json', JSON.stringify(data))
+        await readFunction()
+        res.set('Content-Type', 'application/json')
+        res.json(data[req.params.index])
+    } else if (!found) {
+        res.set('Content-Type', 'text/plain')
+        res.status(404);
+        res.end('Not Found');
+    }
+});
 
 // Variable for PORT 500 if in development
 const PORT = process.env.PORT || 8000;
