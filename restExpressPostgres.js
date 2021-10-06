36f2a823e77256b2db2cad360e5999a0086e81a2
Geraldo Moura <geraldo.moura@ymail.com>
2021-10-06 17:13:52 -0500

    Done

--- restExpressPostgres.js
+++ restExpressPostgres.js
@@ -5,7 +5,7 @@ const pool = require('./db');
 // Get body
 app.use(express.json())
 
-const badRequest = (res) => res.status(400).set('Content-Type', 'text/plain').end('Bad Request');
+const badRequest = (res) => res.status(400).set('Content-Type', 'text/plain').end('Not Found');
 
 /* ROUTES */
 
@@ -25,23 +25,25 @@ app.get('/pets', async (req, res) => {
 
 // POST the data
 app.post('/pets', async (req, res) => {
-    // const { name, age, kind } = req.body
     const pet = {
         name: req.body.name,
         age: req.body.age,
         kind: req.body.kind
     }
-    console.log(pet)
+
+    if (!pet.name || !pet.age || !pet.kind) {
+        return res.status(400).set('Content-Type', 'text/plain').end('Bad Request');
+    }
 
     const createPet = {
-        text: 'INSERT INTO pet(name, age, kind) VALUES ($1, $2, $3)',
+        text: 'INSERT INTO pet(name, age, kind) VALUES ($1, $2, $3) RETURNING *',
         values: [pet.name, pet.age, pet.kind]
     }
 
     pool
         .query(createPet)
         .then(result => result.rows)
-        .then(pet ? badRequest(res) : res.status(200).json(pet))
+        .then(rows => res.status(200).json(rows))
         .catch(e => console.error(e.stack))
 })
 
@@ -55,9 +57,43 @@ app.get('/pets/:index', async (req, res) => {
     pool
         .query(selectIndex)
         .then(result => result.rows)
-        .then(rows => rows ? badRequest(res) : res.status(200).json(rows[0]))
+        .then(rows => rows.length === 0 ? badRequest(res) : res.status(200).json(rows[0]))
         .catch(e => console.error(e.stack))
 });
+
+// PATCH data
+app.patch('/pets/:index', async (req, res) => {
+
+    const updatePet = {
+        text: "UPDATE pet SET name = COALESCE($1, name), age = COALESCE($2, age), kind = COALESCE($3, kind) WHERE id = $4 RETURNING *",
+        values: [req.body.name, req.body.age, req.body.kind, parseInt(req.params.index)]
+    }
+
+    pool
+        .query(updatePet)
+        .then(result => result.rows)
+        .then(rows => rows.length === 0 ? badRequest(res) : res.status(200).json(rows[0]))
+        .catch(e => console.error(e.stack))
+});
+
+// DELETE the data request
+app.delete('/pets/:index', async (req, res) => {
+    const deletePet = {
+        text: 'DELETE FROM pet WHERE id = $1 RETURNING *',
+        values: [parseInt(req.params.index)]
+    }
+
+    pool
+        .query(deletePet)
+        .then(result => result.rows)
+        .then(rows => rows.length === 0 ? badRequest(res) : res.status(200).json(rows[0]))
+        .catch(e => console.error(e.stack))
+});
+
+// 404 Middleware error handler
+app.use((req, res, next) => {
+    res.status(404).set('Content-Type', 'text/plain').end('Not Found')
+})
 
 
 // Variable for PORT 8000 if in development
