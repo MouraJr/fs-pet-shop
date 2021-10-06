2f0ece12b0d85fe86f2357ffb93e25e05ebac6d4
Geraldo Moura <geraldo.moura@ymail.com>
2021-10-06 10:30:10 -0500

    db connection works 🎉🎉

--- db-test.js
+++ db-test.js
@@ -8,18 +8,19 @@ const pool = new Pool({
     port: 5432,
 })
 
-pool.query('SELECT NOW()', (err, res) => {
-    console.log(err, res)
-    pool.end()
-})
-
-// const client = new Client({
-//   user: 'dbuser',
-//   host: 'database.server.com',
-//   database: 'mydb',
-//   password: 'secretpassword',
-//   port: 3211,
+// pool.query('SELECT * FROM pet', (err, res) => {
+//     console.log(err, res)
+//     pool.end()
 // })
+
+
+const client = new Client({
+    user: 'postgres',
+    host: 'localhost',
+    database: 'petshop',
+    password: '112358',
+    port: 5432,
+})
 
 // client.connect()
 // client.query('SELECT NOW()', (err, res) => {
@@ -27,4 +28,4 @@ pool.query('SELECT NOW()', (err, res) => {
 //   client.end()
 // })
 
-module.exports = pool;
+module.exports = pool, client;
